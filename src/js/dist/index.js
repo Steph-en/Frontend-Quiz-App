e519cb1d4b889c7821facb9f82481822c324d1e1
Stephen Appah <appahstephen9@gmail.com>
2024-04-13 17:39:52 +0000

    Feat: Toggle button upgrade

--- src/js/dist/index.js
+++
@@ -1,7 +0,0 @@
-var ToggleBtn = document.getElementById('toggle-btn');
-// const rotate = document.querySelector('.rotate')
-var Main = document.getElementById('main');
-ToggleBtn === null || ToggleBtn === void 0 ? void 0 : ToggleBtn.addEventListener('click', function () {
-    ToggleBtn.classList.remove('rotate');
-    console.log(ToggleBtn);
-});
--- src/js/index.ts
+++
@@ -1,9 +0,0 @@
-const ToggleBtn = document.getElementById('toggle-btn');
-// const rotate = document.querySelector('.rotate')
-const Main = document.getElementById('main');
-
-ToggleBtn?.addEventListener('click', () => {
-    ToggleBtn.classList.remove('rotate');
-    console.log(ToggleBtn);
-    
-})
